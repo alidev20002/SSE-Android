7f2afffa2657531248b9961126c9e86e5006fd6e
alidev20002 <alitigercr71381@gmail.com>
2023-08-12 13:48:03 -0700

    remove unused imports

--- app/src/main/java/com/example/sse_android/MainActivity.kt
+++ app/src/main/java/com/example/sse_android/MainActivity.kt
@@ -1,28 +1,20 @@
 package com.example.sse_android
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import com.example.sse_android.data.models.Message
-import com.example.sse_android.data.network.MessageRemoteDataSource
 import com.example.sse_android.data.network.SSEService
-import com.example.sse_android.data.repository.MessageRepository
 import com.example.sse_android.ui.screen.ChatScreen
 import com.example.sse_android.ui.theme.SSEAndroidTheme
 import com.example.sse_android.ui.viewmodel.MessageViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
