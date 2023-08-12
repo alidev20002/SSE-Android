440a5095935e83c6c5d66e716a71e4a47549c980
alidev20002 <alitigercr71381@gmail.com>
2023-08-12 11:45:21 -0700

    creating list of messages and emit them using flow

--- app/src/main/java/com/example/sse_android/data/network/SSEService.kt
+++ app/src/main/java/com/example/sse_android/data/network/SSEService.kt
@@ -1,14 +1,21 @@
 package com.example.sse_android.data.network
 
-import android.util.Log
 import com.example.sse_android.data.models.Message
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import okhttp3.sse.EventSource
 import okhttp3.sse.EventSourceListener
 import okhttp3.sse.EventSources
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 class SSEService {
@@ -25,18 +32,35 @@ class SSEService {
         .addHeader("Accept", "text/event-stream")
         .build()
 
-    val sseMessagesFlow = MutableStateFlow<List<Message>>(emptyList())
+    private val message = Json.encodeToString(Message("hi"))
 
-    private val sseEventSourceListener = object : EventSourceListener() {
+    private val postMessageRequest = Request.Builder()
+        .method("POST", message.toRequestBody("application/json; charset=utf-8".toMediaType()))
+        .url(POST_MESSAGE_URL)
+        .build()
+
+    val sseMessagesFlow = MutableStateFlow<List<Message>>(emptyList())
+    var messages = mutableListOf<Message>()
+
+    private var sseEventSourceListener = object : EventSourceListener() {
 
         override fun onEvent(eventSource: EventSource, id: String?, type: String?, data: String) {
             super.onEvent(eventSource, id, type, data)
-            Log.i("alitest", "onEvent: data -> $data, type -> $type")
+            if (data.isNotEmpty()) {
+                if (data.startsWith("[") and data.endsWith("]")) {
+                    val msgData: List<Message> = Json.decodeFromString(data)
+                    messages.addAll(msgData)
+                    sseMessagesFlow.tryEmit(messages)
+                } else if (data.startsWith("{") and data.endsWith("}")) {
+                    val msgData: Message = Json.decodeFromString(data)
+                    messages.add(msgData)
+                    sseMessagesFlow.tryEmit(messages)
+                }
+            }
         }
 
         override fun onFailure(eventSource: EventSource, t: Throwable?, response: Response?) {
             super.onFailure(eventSource, t, response)
-            Log.i("alitest", "onEvent: response -> $response, throwable -> $t")
         }
 
     }
@@ -50,7 +74,20 @@ class SSEService {
             .newEventSource(request = sseRequest, listener = sseEventSourceListener)
     }
 
+    fun sendMessage() {
+        sseClient.newCall(postMessageRequest).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle this
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // Handle this
+            }
+        })
+    }
+
     companion object {
         private const val GET_MESSAGES_URL = "http://192.168.0.21:3000/messages"
+        private const val POST_MESSAGE_URL = "http://192.168.0.21:3000/messages"
     }
 }
