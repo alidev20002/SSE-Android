8f4c515a3437ae7e1f9f7d1fa95ede65f128f3c2
alidev20002 <alitigercr71381@gmail.com>
2023-08-12 12:28:09 -0700

    make main activity android entry point

--- app/src/main/java/com/example/sse_android/MainActivity.kt
+++ app/src/main/java/com/example/sse_android/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sse_android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,15 +12,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.sse_android.data.models.Message
+import com.example.sse_android.data.network.MessageRemoteDataSource
 import com.example.sse_android.data.network.SSEService
+import com.example.sse_android.data.repository.MessageRepository
+import com.example.sse_android.ui.screen.ChatScreen
 import com.example.sse_android.ui.theme.SSEAndroidTheme
+import com.example.sse_android.ui.viewmodel.MessageViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val sseService = SSEService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        lifecycleScope.launch {
+//            while (true) {
+//                sseService.sendMessage()
+//                delay(3000)
+//                Log.i("alitest", "onCreate: ${sseService.sseMessagesFlow.value}")
+//            }
+//        }
+
         setContent {
             SSEAndroidTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,25 +45,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ChatScreen(listOf(
+                        Message("hi"),
+                        Message("Hello"),
+                        Message("Good Morning")
+                    ))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SSEAndroidTheme {
-        Greeting("Android")
+        ChatScreen(emptyList())
     }
 }
