2fdc88dc4ccae79c66349ee48cfeaaeddece2c60
alidev20002 <alitigercr71381@gmail.com>
2023-08-12 12:08:57 -0700

    make message view model extend ViewModel Class

--- app/src/main/java/com/example/sse_android/ui/viewmodel/MessageViewModel.kt
+++ app/src/main/java/com/example/sse_android/ui/viewmodel/MessageViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.sse_android.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.sse_android.data.repository.MessageRepository
 
 class MessageViewModel(
     private val messageRepository: MessageRepository
-) {
+): ViewModel() {
     val messages = messageRepository.getMessages()
 }
