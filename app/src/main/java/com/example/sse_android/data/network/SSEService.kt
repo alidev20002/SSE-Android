317abe4f078d6af111622cb21154e8a75690d628
alidev20002 <alitigercr71381@gmail.com>
2023-08-12 10:06:47 -0700

    creating sse GET request for getting messages

--- app/src/main/java/com/example/sse_android/data/network/SSEService.kt
+++ app/src/main/java/com/example/sse_android/data/network/SSEService.kt
@@ -1,6 +1,7 @@
 package com.example.sse_android.data.network
 
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.util.concurrent.TimeUnit
 
 class SSEService {
@@ -9,6 +10,12 @@ class SSEService {
         .connectTimeout(6, TimeUnit.SECONDS)
         .readTimeout(10, TimeUnit.MINUTES)
         .writeTimeout(10, TimeUnit.MINUTES)
+        .build()
+
+    private val sseRequest = Request.Builder()
+        .url(GET_MESSAGES_URL)
+        .header("Accept", "application/json")
+        .addHeader("Accept", "text/event-stream")
         .build()
 
     companion object {
