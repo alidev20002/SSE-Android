18db6adf7104a383af998d5113f26aa8b08ab119
alidev20002 <alitigercr71381@gmail.com>
2023-08-12 09:56:01 -0700

    creating SSEService class for post and get request to sse server

---
+++ app/src/main/java/com/example/sse_android/data/network/SSEService.kt
@@ -0,0 +1,8 @@
+package com.example.sse_android.data.network
+
+class SSEService {
+
+    companion object {
+        private const val GET_MESSAGES_URL = "http://192.168.0.21:3000/messages"
+    }
+}
