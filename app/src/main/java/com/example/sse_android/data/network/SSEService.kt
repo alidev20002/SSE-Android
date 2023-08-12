73371e93f5771bbdf8c76dd2738839b2c8e0515d
alidev20002 <alitigercr71381@gmail.com>
2023-08-12 09:58:22 -0700

    creating sse Client

--- app/src/main/java/com/example/sse_android/data/network/SSEService.kt
+++ app/src/main/java/com/example/sse_android/data/network/SSEService.kt
@@ -1,6 +1,15 @@
 package com.example.sse_android.data.network
 
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
 class SSEService {
+
+    private val sseClient = OkHttpClient.Builder()
+        .connectTimeout(6, TimeUnit.SECONDS)
+        .readTimeout(10, TimeUnit.MINUTES)
+        .writeTimeout(10, TimeUnit.MINUTES)
+        .build()
 
     companion object {
         private const val GET_MESSAGES_URL = "http://192.168.0.21:3000/messages"
